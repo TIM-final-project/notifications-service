faac0baae09e7c64e1489825735182471748cea4
Martin <martin.belcic@gmail.com>
2023-01-08 15:57:34 -0300

    Lo cambie pero no lo testee, suerte

--- src/notifications/services/exceptions.service.ts
+++ src/notifications/services/exceptions.service.ts
@@ -1,12 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { RpcException } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IsNull, Repository } from 'typeorm';
-import { ExceptionCreateDTO } from '../dtos/exception/exception-create.dto';
 import { ExceptionUpdateDTO } from '../dtos/exception/exception-update.dto';
 import { ExceptionDTO } from '../dtos/exception/exception.dto';
 import { ExceptionEntity } from '../entities/exception.entity';
-import { States } from '../enum/States.enum';
 import { ExceptionQPs } from '../qps/exception.qps';
 
 @Injectable()
@@ -34,39 +31,13 @@ export class ExceptionsService {
     return this.exceptionsRepository.find(query);
   }
 
-  // async create(exceptionDTO: ExceptionCreateDTO): Promise<ExceptionDTO> {
-  //   const where = {
-  //     driverId: exceptionDTO.driverId,
-  //     vehicleId: exceptionDTO.vehicleId,
-  //     state: States.PENDING.toString()
-  //   };
-  //   const exceptions = await this.exceptionsRepository.find({ where });
-  //   if (exceptions?.length) {
-  //     throw new RpcException({
-  //       message:
-  //         'Ya se solicito una excepcion para este conductor y vehiculo. Por favor espere a que esta sea resuelta.'
-  //     });
-  //   }
-
-  //   try {
-  //     const exceptionEntity: ExceptionEntity = exceptionDTO;
-  //     this.logger.debug('Exception Entity ', exceptionEntity);
-  //     return this.exceptionsRepository.save(exceptionEntity);
-  //   } catch (error) {
-  //     this.logger.error('Error creating Exception', error);
-  //     throw new RpcException({
-  //       message: 'Ha ocurrido un error al pedir la excepcion.'
-  //     });
-  //   }
-  // }
-
   async update(
     id: number,
     updateExceptionDTO: ExceptionUpdateDTO
   ): Promise<ExceptionDTO> {
     const exception: ExceptionEntity = await this.exceptionsRepository.findOne(
       id,
-      {relations: ['arrival']}
+      { relations: ['arrival'] }
     );
     this.logger.debug('Exception', { exception });
     this.exceptionsRepository.merge(exception, updateExceptionDTO);
